94b9a7c18bb7495dcb180e7ba125c025e8ba1d35
Seb Gertz <sebgertz@gmail.com>
2018-08-23 19:12:29 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,8 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+      @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = 'client_id'
   end
 end
