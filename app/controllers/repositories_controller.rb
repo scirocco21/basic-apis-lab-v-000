07181216ede8d0566ed5ea944a286610de735786
Seb Gertz <sebgertz@gmail.com>
2018-08-23 19:38:32 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,7 +7,7 @@ class RepositoriesController < ApplicationController
   def github_search
     begin
       @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-        req.params[:q] = query
+        req.params[:q] = params[:query]
       end
       body = JSON.parse(@resp.body)
       if @resp.success?
